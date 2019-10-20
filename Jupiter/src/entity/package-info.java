ce08dfd887eecb85b5bdd227f147e6dbf46876d9
sheerryZ <48262654+sheerryZ@users.noreply.github.com>
2019-10-20 02:28:00 -0400

    Add files via upload

---
+++ Jupiter/src/entity/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author shuya
+ *
+ */
+package entity;
---
+++ Jupiter/src/recommendation/GeoRecommendation.java
@@ -0,0 +1,58 @@
+package recommendation;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import db.mysqul.MySQLConnection;
+import entity.Item;
+
+//Recommendation based on geo distance and similar categories
+public class GeoRecommendation {
+	 public List<Item> recommendItems(String userId, double lat, double lon) {
+			List<Item> recommendedItems = new ArrayList<>();//result
+
+			// get all favorited itemids
+			DBConnection connection = DBConnectionFactory.getConnection();
+			Set<String> favoritedItemIds = connection.getFavoriteItemIds(userId);
+
+			// get all custom interested items' categories,  sort by count
+			// {"sports": 5, "music": 3, "art": 2}
+			Map<String, Integer> allCategories = new HashMap<>();
+			for (String itemId : favoritedItemIds) {
+				Set<String> categories = connection.getCategories(itemId);
+				for (String category : categories) {
+					allCategories.put(category, allCategories.getOrDefault(category, 0) + 1);//0 is default value
+				}
+			}
+			
+			//get top interested category
+			List<Entry<String, Integer>> categoryList = new ArrayList<>(allCategories.entrySet());
+			Collections.sort(categoryList, (Entry<String, Integer> e1, Entry<String, Integer> e2) -> {
+				return Integer.compare(e2.getValue(), e1.getValue());
+			});
+			
+			// search based on category, filter out favorite items
+			Set<String> visitedItemIds = new HashSet<>();
+			for (Entry<String, Integer> category : categoryList) {
+				List<Item> items = connection.searchItems(lat, lon, category.getKey());
+				
+				for (Item item : items) {
+					if (!favoritedItemIds.contains(item.getItemId()) && !visitedItemIds.contains(item.getItemId())) {
+						recommendedItems.add(item);
+						visitedItemIds.add(item.getItemId());
+					}
+				}
+			}
+			
+			connection.close();
+			return recommendedItems;
+}
+
+}
